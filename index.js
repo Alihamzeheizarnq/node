bcf5139e3cd733f9f418058910865187f1786651
alihamzehei <alihamzehei2017@gmail.com>
2023-11-04 21:55:12 +0330

    feat:udpated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello There HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
+  res.send('Hello There Ali')
 })
 
 app.listen(port, () => {
